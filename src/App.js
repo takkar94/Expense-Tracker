9a21db96fe84f70caf7bf25972034d82a0ab116d
takkar94 <ghimireaditya@gmail.com>
2024-05-29 11:26:25 +0545

    adding components and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+      </div>
+    </>
   );
 }
 
