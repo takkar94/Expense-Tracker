ab111ef4a995326ff2f2387a6b59704d6db1a9ca
takkar94 <ghimireaditya@gmail.com>
2024-05-29 12:13:32 +0545

    created context and made the value dynamic

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import Transaction from './Transaction';
 import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
@@ -7,11 +8,7 @@ const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transactions => (
-             <li className="minus">
-             {transactions.text} <span>-$400</span><button className="delete-btn">x</button>
-           </li> 
-        ))}
+        {transactions.map(transaction => (<Transaction key = {transaction.id} transaction = {transaction}/>))}
       </ul>
     </>
   )
